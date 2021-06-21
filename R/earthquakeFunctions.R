116e6fe9ee6c93f1b37c88c80efd123973be61fa
lilyacb <lily-clough@utuls.edu>
2021-06-21 11:42:03 -0500

    geo_circle_intersections

--- R/earthquakeFunctions.R
+++ R/earthquakeFunctions.R
@@ -8,7 +8,7 @@
 #' @param starttime attribute starttime
 #' @param sta station code
 #' @return ggplot of the seismic data
-#' @example 
+#' @examples 
 #' Usage example
 #' @export
 plot3compEqData<-function(data.df,total.time=27,plotTitle="plot",mag="mag",starttime="starttime",sta="sta"){ # time in benchmark dataset always 27s
@@ -115,4 +115,94 @@ circle <- function(center = c(0,0),diameter = 1, npoints = 100){
   xx <- center[1] + r * cos(tt)
   yy <- center[2] + r * sin(tt)
   return(data.frame(x = xx, y = yy))
+}
+
+
+#' geo_circle_intersections: use points on two circles (can be created with destPoint) to approximate their intersection points by
+#' finding points on each circle that are close together (using distGeo for accurate distance). This functino uses 
+#' hclust to separate out the two intersections by geographical distance.
+#' @param circle1 dataframe of points on a circle with cols (lon,lat)
+#' @param circle2 dataframe of points on a circle with cols (lon,lat)
+#' @param intersection_name character string to label the intersection points
+#' @return a dataframe of cols (lon,lat) and 2 rows containing the 2 intersection points of the circles (as averages of all 
+#' the points in a cluster), if they exist
+#' @examples 
+#' Usage example
+#' RLOK_ELIS_ints<-geo_circle_intersections(RLOK_circle,ELIS_circle,"RLOK_ELIS")
+#' @export
+geo_circle_intersections<-function(circle1,circle2,intersection_name){
+  int1<-c()
+  int2<-c()
+  ints<-c()
+  ints.df<-c()
+  for(i in seq(1:dim(circle1)[1])){ #write parallel func???!!!
+   # identifier_1_files_ind<-pvec(seq_along(files),function(i)
+    #  grepl(identifier_1,files[i],fixed=T),mc.cores=cores)
+    for(j in seq(1:dim(circle2)[1])){
+      dg<-distGeo(circle1[i,],circle2[j,])/1000<=2.5 # within 2.5 km
+      if(dg){
+        print("found intersection")
+        int<-rbind(circle1[i,],circle2[j,])
+        ints<-rbind(ints,int)
+      }
+    }
+  }
+  
+  # check for contents of ints--if length=0 then exit
+  stopifnot(length(ints)>0)
+  # change ints to a dataframe
+  ints.df<-as.data.frame(ints)
+  colnames(ints.df)<-c("intersection_longitude","intersection_latitude")
+  #print(ints.df)
+  
+  # parse out the different intersections (some sets of points may represent the same intersection)
+  # find where the intersections are different. Create a distance matrix to determine geographical distance
+  int_dist<-distm(ints.df,ints.df)/1000 #km
+  int_dist.df<-as.data.frame(int_dist)
+  # names need to be different
+  rnames<-c()
+  for(i in seq(1:nrow(int_dist.df))){
+    rnames<-c(rnames,paste(intersection_name,"_",i,sep=""))
+  }
+  #print(rnames)
+  rownames(int_dist.df)<-rnames
+  colnames(int_dist.df)<-rnames
+  int_dist.dist<-as.dist(int_dist.df)
+  
+  # hclust to find clusters of points
+  clust<-hclust(int_dist.dist)
+  clust$labels<-rnames
+  int_dist.dendro<-clust %>% as.dendrogram() %>% set("labels_to_character")
+  plot(int_dist.dendro)
+  # grab each clade--get the names and use names to grab the points in the intersection dataframe
+  geoCuts<-cutree(int_dist.dendro,h=10)
+  # look at the cluster groupings
+  print(table(geoCuts))
+  # get first group
+  first_group<-names(which(geoCuts==1))
+  #print(first_group)
+  # get second group---add check for tangenecy??
+  second_group<-names(which(geoCuts==2))
+  #print(second_group)
+  # take the average of each set of (lon, lat) (could do something else?)
+  # grab the correct points from the int.df that contains the coords
+  cutoff<-length(first_group)
+  first_group_ints<-ints.df[1:cutoff,]
+  print(first_group_ints)
+  #print(dim(first_group_ints))
+  second_group_ints<-ints.df[(cutoff+1): nrow(ints.df),]
+  print(second_group_ints)
+  # find the first average
+  first_lon_avg<-mean(first_group_ints[,1])
+  first_lat_avg<-mean(first_group_ints[,2])
+  final_intersection_coords1<-(c(first_lon_avg,first_lat_avg))
+  # find the second average
+  second_lon_avg<-mean(second_group_ints[,1])
+  second_lat_avg<-mean(second_group_ints[,2])
+  final_intersection_coords2<-(c(second_lon_avg,second_lat_avg))
+  final_int.df<-as.data.frame(rbind(final_intersection_coords1,final_intersection_coords2))
+  rownames(final_int.df)<-c(paste(intersection_name,".1",sep=""),paste(intersection_name,".2",sep="")) #change to intersection names
+  colnames(final_int.df)<-c("lon","lat")
+  print(final_int.df)
+  return(final_int.df)
 }
